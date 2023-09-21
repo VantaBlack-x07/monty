04cb787db261992f922326ec47ebcf2db09bc33a
mohamed gomaa <mo7amed.gom3a.7moda@gmail.com>
2023-09-21 15:34:24 -0700

    fix

--- bytecodes/001.m
+++ bytecodes/001.m
@@ -1,15 +1,12 @@
-push 0 Push 0 onto the stack$
-push 1 Push 1 onto the stack$
-$
-push 2$
-  push 3$
-                   pall    $
-$
-$
-                           $
-push 4$
-$
-    push 5    $
-      push    6        $
-$
-pall This is the end of our program. Monty is awesome!$
+push 0
+push 1
+push 52 fsdg
+  push 3
+  
+                   pall    
+push 4
+    push 5   
+    gdgfr gdf g 
+      push    6        
+pall
+gdgfr gdf g
